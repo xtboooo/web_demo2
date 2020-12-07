6e11ccccbbd1c6a74034bed2319ac7115f6238ea
xtbo97 <xtbo9719@gmail.com>
2020-12-07 19:41:07 +0800

    注册视图接受post数据

--- users/views.py
+++ users/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render, HttpResponse
 
 def register(request):
     """注册 View 视图函数"""
-    return render(request, 'register.html')
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    else:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(f'username:{username} password:{password}')
+        return HttpResponse('注册成功')
