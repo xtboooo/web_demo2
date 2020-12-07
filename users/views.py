f12e23b45dd05f0c3353992a0ff470ed244e9529
xtbo97 <xtbo9719@gmail.com>
2020-12-07 19:35:33 +0800

    注册视图页面模板

--- users/views.py
+++ users/views.py
@@ -3,18 +3,4 @@ from django.shortcuts import render, HttpResponse
 
 def register(request):
     """注册 View 视图函数"""
-    html = """
-    <html>
-        <head>
-            <title>注册页面</title>
-        </head>
-        <body>
-            <form method='post' action='/register/'>
-                username：<input type='text' name='username' /><br/>
-                password：<input type='password' name='password' /><br/>
-                <input type='submit' value='注册' />
-            </form>
-        </body>
-    </html>
-    """
-    return HttpResponse(html)
+    return render(request, 'register.html')
