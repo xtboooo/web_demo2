15166df172ce385fba87f720f2475b0c58035974
xtbo97 <xtbo9719@gmail.com>
2020-12-09 18:21:24 +0800

    用户信息视图url请求参数传递返回

--- users/views.py
+++ users/views.py
@@ -9,15 +9,20 @@ def register(request):
     if request.method == 'GET':
         return render(request, 'register.html', )
     else:
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        # username = request.POST.get('username')
+        # password = request.POST.get('password')
+        import json
+        req_dict = json.loads(request.body)
+        username = req_dict.get('username')
+        password = req_dict.get('password')
         print(f'username:{username} password:{password}')
 
         # 先导入get_md5_pwd, 传入password, 获取md5加密后的密码
         # password = get_md5_pwd(password)
 
         user = User.objects.create(username=username, password=password)
-        return redirect('/login/')
+        # return redirect('/login/')
+        return HttpResponse('注册成功')
 
 
 def login(request):
@@ -40,3 +45,20 @@ def login(request):
             if remember == 'true':
                 request.session.set_expiry(0)
             return JsonResponse({'message': 'login success'})
+
+
+def user_info(request, id):
+    try:
+        user = User.objects.get(id=id)
+    except User.DoesNotExist:
+        return JsonResponse({'message': '用户不存在'})
+    else:
+        res_data = {
+            'id': user.id,
+            'name': user.username,
+            'gender': user.gender,
+            'age': user.age,
+            'mobile': user.mobile
+        }
+
+    return JsonResponse(res_data)
